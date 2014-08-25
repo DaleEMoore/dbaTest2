faa68c10a9ee9fd93de3e4a7ef982d3a9ac6f1f3
Dale E. Moore <DaleEMoore@gMail.Com>
2014-08-25 15:44:34 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# dbaTest2 for using https://github.com/nathanborror/django-basic-apps
+# Django Basic Apps (DBA)
+# Download https://github.com/nathanborror/django-basic-apps
+# unarchive the download archive; cd into folder.
+# python setup.py install
+
+# sudo apt-get install python-django
+# django-admin startproject dbaTest2
+# cd dbaTest2/
+# python manage.py runserver 0.0.0.0:8000
+# http://localhost:8000
+# vi dbaTest2/settings.py
+# python manage.py syncdb
+
+# copy djang-basic-apps/basic/ in
+# python manage.py runserver 0.0.0.0:8000
+# http://localhost:8000
+
+__author__ = 'dalem'
+
